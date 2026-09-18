1a8e5c68077bbaf011d4d058241d35905bfe8a57
raul <00025226@uca.edu.sv>
2026-09-18 14:27:57 -0600

    feat/Finalizado el calculo de la distancia y la clasificacion de la distancia del origen

--- main.cpp
+++ main.cpp
@@ -12,6 +12,13 @@ struct PuntoTrayectoria
     string clasificacion;
 };
 
+float calcularDistancia(PuntoTrayectoria *punto)
+{
+    float distancia = (pow(punto->coordenadas[0], 2) + pow(punto->coordenadas[1], 2) + pow(punto->coordenadas[2], 2));
+
+    punto->distanciaOrigen = distancia;
+    return distancia;
+}
 
 void registrarPunto(PuntoTrayectoria &punto)
 {
@@ -30,6 +37,31 @@ void registrarPunto(PuntoTrayectoria &punto)
     cout << "Cual es la coordenada Z del punto? :" << endl;
     cin >> punto.coordenadas[2];
     cout << endl;
+    cout << "Distancia origen: " << calcularDistancia(&punto) << endl;
+}
+
+void clasificarPunto(PuntoTrayectoria &punto)
+{
+    float distancia = punto.distanciaOrigen;
+
+    if (distancia >= 0 && distancia <= 5)
+    {
+        punto.clasificacion = "CERCANO";
+    }
+    else if (distancia > 5 && distancia <= 10)
+    {
+        punto.clasificacion = "INTERMEDIO";
+    }
+    else if (distancia > 10 && distancia <= 20)
+    {
+        punto.clasificacion = "LEJANO";
+    }
+    else if (distancia > 20)
+    {
+        punto.clasificacion = "EXTREMO";
+    }
+
+    cout << "Clasificacion de la distancia del punto: " << punto.clasificacion << endl;
 }
 
 int main()
@@ -53,6 +85,8 @@ int main()
         cout << i + 1 << ". Punto: " << endl;
         cout << endl;
         registrarPunto(puntos[i]);
+        cout << endl;
+        clasificarPunto(puntos[i]);
     }
     return 0;
 }
